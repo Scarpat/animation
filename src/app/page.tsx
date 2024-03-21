3e546559a2cc0fef4fb5a978ff047d358e483866
Gabriel Scarpat <scarpatgabriel@gmail.com>
2024-03-21 15:12:05 -0300

    waves

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,21 @@
+/* eslint-disable jsx-a11y/alt-text */
+import  waveFront from '@/assets/wave.svg'
+import  waveBack from '@/assets/wave-bg.svg'
+import Image from 'next/image'
+
 export default function Home (){
   return (
     <>
-    <div className="overflow-hidden">
-
-    <div className="w-screen h-screen bg-black absolute top-0 left-0 "/>
-    <div className="w-screen h-screen bg-black flex items-center justify-center blur-lg  contrast-[300] animate-spin-slow">
-
-    <div className="w-40 h-40 bg-blue-300/75 rounded-full relative z-[5]"/>
-    <div className="w-20 h-20 bg-pink-400 shadow-xl  rounded-full absolute animateRun1"/>
-    <div className="w-20 h-20 bg-yellow-400 rounded-full absolute animateRun2"/>
-    <div className="w-20 h-20 bg-green-400 rounded-full absolute animateRun3"/>
-
-    </div>
+    <div className="bg-black w-screen h-screen flex">
+      <div className="bg-white w-[20vw] h-[20vw] rounded-full m-auto absolute inset-0 overflow-hidden flex items-center justify-center">
+        <div className='w-[200vw] h-screen'>
+        <Image src={waveBack.src} width={waveBack.width} height={waveBack.height} className='max-w-none w-[100vw]  max-h-none relative wave-animation-2' alt={''}/>
+       <div className='relative wave-animation-1 -mt-[400px]'>
+        <Image src={waveFront.src} width={waveFront.width} height={waveFront.height} className='max-w-none relative h- z-10 -mb-10 w-[100vw] max-h-none ' alt={''}/>
+        <div className='bg-[#0099ff] h-screen'/>
+       </div>
+        </div>
+      </div>  
     </div>
     </>
   )
---
+++ src/components/animation-orbit.tsx
@@ -0,0 +1,16 @@
+export default function Orbits (){
+    <>
+    <div className="overflow-hidden">
+
+    <div className="w-screen h-screen bg-black absolute top-0 left-0 "/>
+    <div className="w-screen h-screen bg-black flex items-center justify-center blur-lg  contrast-[300] animate-spin-slow">
+
+    <div className="w-40 h-40 bg-blue-300/75 rounded-full relative z-[5]"/>
+    <div className="w-20 h-20 bg-pink-400 shadow-xl  rounded-full absolute animateRun1"/>
+    <div className="w-20 h-20 bg-yellow-400 rounded-full absolute animateRun2"/>
+    <div className="w-20 h-20 bg-green-400 rounded-full absolute animateRun3"/>
+
+    </div>
+    </div>
+    </>
+}
